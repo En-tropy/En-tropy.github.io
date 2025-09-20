e72e0d48a3e1b547ea89affc0918a377be229d71
En-tropy <72965457+En-tropy@users.noreply.github.com>
2025-09-20 06:33:20 +0000

    Deploying to gh-pages from @ En-tropy/En-tropy.github.io@ea85e51c985911cef059a3065ff47fb85e8efccc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -43,16 +43,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-my-first-first-author-paper-has-been-accepted-to-isca-2025-tada",
+          title: 'My first first-author paper has been accepted to ISCA 2025! :tada:',
           description: "",
           section: "News",},{
         id: 'social-email',
